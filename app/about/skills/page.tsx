cc4969760e214beb6fa853bf6740e651dc76926d
Jihyeon Suh <suhjh0222@naver.com>
2026-01-06 16:45:34 +0900

    파일 기반 라우팅

---
+++ app/about/skills/page.tsx
@@ -0,0 +1,18 @@
+export default function AboutSkills() {
+    return (
+        // min-h-screen으로 전체 높이 보장, grid로 3개 영역 분할
+        <div className='flex min-h-screen flex-col'>
+            {/* Main 영역 */}
+            <main className='flex-1'>
+                <div className='container mx-auto px-4 py-8'>
+                    <div className='space-y-8'>
+                        {/* 섹션 제목 */}
+                        <h2 className='text-3xl font-bold tracking-tight'>
+                            소개
+                        </h2>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+}
---
+++ components/layouts/Header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+export default function Header() {
+    return (
+        /* Header 영역 */
+        <header className='sticky top-0 z-50 border-b bg-background'>
+            <div className='container mx-auto flex h-14 items-center px-4'>
+                <Link href='/' className='text-xl font-semibold'>
+                    <span className='font-bold'>짐코딩 블로그</span>
+                </Link>
+                <nav className='ml-auto flex items-center gap-4'>
+                    <Link href='/' className='hover:text-primary font-medium'>
+                        홈
+                    </Link>
+                    <Link
+                        href='blog'
+                        className='hover:text-primary font-medium'
+                    >
+                        블로그
+                    </Link>
+                    <Link
+                        href='/about'
+                        className='hover:text-primary font-medium'
+                    >
+                        소개
+                    </Link>
+                </nav>
+            </div>
+        </header>
+    );
+}
